aa6e4aaf77037cb841b88066fc6388cf7232be6e
Preston <preston.schwartz505@gmail.com>
2020-07-20 17:02:48 -0500

    Added calendar and gratitude list, add profile filler.

--- src/scenes/account/index.tsx
+++ src/scenes/account/index.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
-import { SafeAreaView, Text, TouchableHighlight } from 'react-native';
+import { SafeAreaView, Text, TouchableHighlight, View } from 'react-native';
+import { Avatar, Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const AccountScreen = ({navigation}:any) => (
-  <SafeAreaView>
-    <Text>Screen: Account</Text>
 
-    <TouchableHighlight onPress={() => navigation.navigate('Login')}>
-      <Text>Go to Login</Text>
-    </TouchableHighlight>
-  </SafeAreaView>
+const AccountScreen = ({ navigation }: any) => (
+  // <SafeAreaView>
+  <View style={{ flexDirection: 'column', justifyContent: 'center' }}>
+    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+      <Avatar
+        size="xlarge"
+        rounded
+        source={{
+          uri:
+            'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        }}
+        onPress={() => console.log("Works!")}
+        activeOpacity={0.7}
+        containerStyle={{ marginTop: 115, marginBottom: 75 }}
+      />
+    </View>
+
+    <Input
+      value='Preston Schwartz'
+      leftIcon={
+        <Icon
+          name='user'
+          size={24}
+          color='black'
+        />
+      }
+    />
+    <Input
+      value='test@test.com'
+      leftIcon={
+        <Icon
+          name='envelope'
+          size={24}
+          color='black'
+        />
+      }
+    />
+    <Input
+      value='555-555-5555'
+      leftIcon={
+        <Icon
+          name='phone'
+          size={24}
+          color='black'
+        />
+      }
+    />
+  </View>
+  // </SafeAreaView>
 );
 
 export default AccountScreen;
--- src/scenes/gratitude/index.tsx
+++ src/scenes/gratitude/index.tsx
@@ -1,34 +1,93 @@
 import React from 'react';
-import { SafeAreaView, Text, TouchableHighlight } from 'react-native';
+import { SafeAreaView, Text, TouchableHighlight, View } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
-import { ButtonGroup } from 'react-native-elements';
 
-export class Gratitude extends React.Component<{}, { selectedIndex: number; }> {
+import { ButtonGroup, ListItem, Input } from 'react-native-elements';
+
+export class Gratitude extends React.Component<{}, { selectedIndex: number; calendarViewIsVisible: boolean, dailyViewIsVisible: boolean; }> {
 
   constructor(props: any) {
     super(props);
     this.state = {
-      selectedIndex: 1
+      selectedIndex: 1,
+      calendarViewIsVisible: false,
+      dailyViewIsVisible: true
     };
     this.updateIndex = this.updateIndex.bind(this);
   }
 
-  updateIndex(selectedIndex: number) {
-    this.setState({ selectedIndex });
+  updateIndex(index: any) {
+    if (index === 0) {
+      this.setState({ selectedIndex: index, calendarViewIsVisible: true, dailyViewIsVisible: false });
+    }
+    else {
+      this.setState({ selectedIndex: index, calendarViewIsVisible: false, dailyViewIsVisible: true });
+
+    }
+    console.log('updateIndex: %o', index);
   }
 
   render() {
     const buttons = ['Calendar', 'Daily'];
     const { selectedIndex } = this.state;
 
-    return (
-      <ButtonGroup
-        onPress={this.updateIndex}
-        selectedIndex={selectedIndex}
-        buttons={buttons}
-        containerStyle={{ height: 30 }}
-      />
-    );
+    if (this.state.calendarViewIsVisible) {
+      return (
+        <View>
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{ height: 30 }}
+          />
+          <View>
+            <Calendar
+              // Collection of dates that have to be marked. Default = {}
+              markedDates={{
+                '2012-05-16': { selected: true, marked: true, selectedColor: 'blue' },
+                '2012-05-17': { marked: true },
+                '2012-05-18': { marked: true, dotColor: 'red', activeOpacity: 0 },
+                '2012-05-19': { disabled: true, disableTouchEvent: true }
+              }}
+            />
+          </View>
+        </View>
+
+      );
+    } else {
+      return (
+        <View>
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{ height: 30 }}
+          />
+          <View>
+            <Input
+              placeholder='Add Daily GRATITUDE'
+            />
+            <Input
+              value='I am thankful for the opportunity to create.'
+            />
+            <Input
+              value='I am thankful for the opportunity to breathe.'
+            />
+            <Input
+              value='I am thankful for the opportunity to be me.'
+            />
+            <Input
+              value='I am thankful for the opportunity to serve.'
+            />
+            <Input
+              value='I am thankful for the opportunity to learn.'
+            />
+          </View>
+        </View>
+      );
+    }
+
   }
 }
 
